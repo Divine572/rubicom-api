e79ceb0f9dbed7acce2b73f3a7000256c28259bc
divicodes <divinef837@gmail.com>
2022-07-28 12:41:36 +0100

    added comment handlers

---
+++ src/controllers/comment.controllers.js
@@ -0,0 +1,80 @@
+const Comment = require('../models/comment.model');
+const AppError = require('../utils/error.utils');
+const catchAsync = require('../utils/catchAsync.utils');
+const APIFeatures = require('../utils/apiFeature.utils');
+
+exports.createComment = catchAsync(async (req, res, next) => {
+  const comment = await Comment.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      data: comment,
+    },
+  });
+});
+
+exports.getAllComments = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Comment.find(), req.query)
+    .filter()
+    .limitFields()
+    .sort()
+    .paginate();
+
+  const comments = await features.query;
+  res.status(201).json({
+    status: 'success',
+    result: comments.length,
+    data: {
+      data: comments,
+    },
+  });
+});
+
+exports.getComment = catchAsync(async (req, res, next) => {
+  const comment = await Comment.findById(req.params.id);
+
+  if (!comment) {
+    return next(new AppError('No comment found with this id', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: comment,
+    },
+  });
+});
+
+exports.updatePost = catchAsync(async (req, res, next) => {
+  const comment = await Comment.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+
+  if (!comment) {
+    return next(new AppError('No comment found with this id', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: comment,
+    },
+  });
+});
+
+exports.deletePost = catchAsync(async (req, res, next) => {
+  const comment = await Comment.findByIdAndDelete(req.params.id);
+
+  if (!comment) {
+    return next(new AppError('No comment found with this id', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: {
+      data: null,
+    },
+  });
+});
